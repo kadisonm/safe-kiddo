2e447c6307283a2adc80598f2a710f4d00f9a2e0
Kadison <134670047+KadisonM@users.noreply.github.com>
2024-09-15 22:34:01 +0930

    Add download blocking

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,2 +1,18 @@
-console.log('This is the background page.');
-console.log('Put the background scripts here.');
+import browser from 'webextension-polyfill';
+
+async function canBlockDownload() {
+    const result = await browser.storage.local.get('blockDownloads');
+    return result.blockDownloads || false;
+}
+
+browser.downloads.onCreated.addListener(async (downloadItem) => {
+    if (await canBlockDownload()) {
+        await browser.downloads.cancel(downloadItem.id);
+        await browser.notifications.create({
+        type: "basic",
+        iconUrl: "icon-34.png",
+        title: "Download Blocked",
+        message: `The download was blocked.`
+        });
+    }
+});
--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,26 +1,51 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
 import logo from '../../assets/img/logo.svg';
+
+import browser from 'webextension-polyfill';
+
 import './Popup.css';
 
-const Popup = () => {
+export default function Popup() {
+
+  const [blockDownloads, setBlockDownloads] = useState(false);
+
+  async function fetchBlockDownloads() {
+    try {
+      const result = await browser.storage.local.get('blockDownloads');
+      setBlockDownloads(result.blockDownloads || false);
+    } catch (error) {
+      console.error('Error fetching blockDownloads:', error);
+    }
+  };
+
+  fetchBlockDownloads()
+
+  useEffect(() => {
+    fetchBlockDownloads();
+  }, []);
+
+  const handleToggleChange = async (event) => {
+    const isEnabled = event.target.checked;
+    try {
+      await browser.storage.local.set({ blockDownloads: isEnabled });
+      setBlockDownloads(isEnabled);
+    } catch (error) {
+      console.error('Error setting blockDownloads:', error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Test
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-      </header>
+    <div id="app-container">
+      <label>
+        <input
+          type="checkbox"
+          checked={blockDownloads}
+          onChange={handleToggleChange}
+          id="toggle"
+        />
+        Block Downloads
+      </label>
     </div>
   );
 };
-
-export default Popup;
